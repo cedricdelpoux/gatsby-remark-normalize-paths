b69de64cf39918ca3b20c95bd5e904c0f96d087d
ncphillips <ncphillips.19@gmail.com>
2018-12-14 12:26:48 -0400

    asserted some more test cases

--- src/gatsby-node.test.js
+++ src/gatsby-node.test.js
@@ -40,11 +40,40 @@ describe("gatsby-node", () => {
 
     it("should convert any string starting with '/' into a relative path", () => {
       utils.getRelativePath.mockImplementation(() => "./something.jpg")
-      const node = new Node({not_an_imag: "/something"})
+      const node = new Node({pic: "/something"})
 
       onCreateNode({node}, pluginOptions)
 
-      expect(node.frontmatter).toEqual({not_an_imag: "./something.jpg"})
+      expect(node.frontmatter).toEqual({pic: "./something.jpg"})
+    })
+
+    it("should convert fields objects", () => {
+      utils.getRelativePath.mockImplementation(() => "./something.jpg")
+      const node = new Node({author: {portrait: "/something"}})
+
+      onCreateNode({node}, pluginOptions)
+
+      expect(node.frontmatter).toEqual({author: {portrait: "./something.jpg"}})
+    })
+
+    it("should convert arrays of paths", () => {
+      utils.getRelativePath.mockImplementation(() => "./something.jpg")
+      const node = new Node({gallery: ["/something", "/toot"]})
+
+      onCreateNode({node}, pluginOptions)
+
+      expect(node.frontmatter).toEqual({gallery: ["/something", "/toot"]})
+    })
+
+    it("should not convert fields of objects nested in arrays", () => {
+      utils.getRelativePath.mockImplementation(() => "./something.jpg")
+      const node = new Node({author: [{portrait: "/something"}]})
+
+      onCreateNode({node}, pluginOptions)
+
+      expect(node.frontmatter).not.toEqual({
+        authors: [{portrait: "./something.jpg"}],
+      })
     })
   })
 })
