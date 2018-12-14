2ba0a0d06af3f4da60f45ea08d3724605b7c5852
ncphillips <ncphillips.19@gmail.com>
2018-12-14 12:10:23 -0400

    test that it changes toplevel path fields to relative urls

--- src/gatsby-node.test.js
+++ src/gatsby-node.test.js
@@ -1,18 +1,42 @@
 const {onCreateNode} = require("./gatsby-node")
+const utils = require("./utils")
+
+jest.mock("./utils.js")
 
 describe("gatsby-node", () => {
+  const pluginOptions = {pathFields: ["image"]}
+
   it("should not change a node without frontmatter", () => {
-    const pluginOptions = {pathFields: []}
-    const node = {
-      fileAbsolutePath: "test",
-      frontmatter: {},
-      internal: {
-        type: "MarkdownRemark",
-      },
-    }
+    const node = new Node()
 
     onCreateNode({node}, pluginOptions)
 
     expect(node.frontmatter).toEqual({})
   })
+
+  it("should change top-level field marked as a path", () => {
+    const node = new Node({image: "/path/to/image.jpg"})
+    const relativePath = "./path/to/image.jpg"
+    utils.getRelativePath.mockImplementation(() => relativePath)
+
+    onCreateNode({node}, pluginOptions)
+
+    expect(node.frontmatter).toEqual({image: relativePath})
+  })
+
+  it("should not change top-level field that's not been marked as a field", () => {
+    const node = new Node({not_an_image: "/path/to/image.jpg"})
+
+    onCreateNode({node}, pluginOptions)
+
+    expect(node.frontmatter).toEqual({not_an_image: "/path/to/image.jpg"})
+  })
 })
+
+class Node {
+  constructor(frontmatter = {}) {
+    this.frontmatter = frontmatter
+    this.fileAbsolutePath = "some/file/path.md"
+    this.internal = {type: "MarkdownRemark"}
+  }
+}
