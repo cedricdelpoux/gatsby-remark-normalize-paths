7e6c059780ac71c5a7452091e6aee86319266b1b
DJ <dj@walker.dev>
2020-09-27 03:18:54 -0400

    Add better Objects nested in arrays support
    * add smoke test
    
    * first test
    
    * test that it changes toplevel path fields to relative urls
    
    * add tests distinguishing behaviour when pathFields does and does not exist
    
    * asserted some more test cases
    
    * fix test case
    
    * fix tests and flip last test
    
    * refactor into recusive strategy
    
    * remove each-props
    
    * add no-op return
    
    * npm audit fix
    
    * update package name
    
    * Delete package-lock.json
    
    * Update package.json
    
    * Delete yarn.lock
    
    * Create yarn.lock
    
    * Update package.json
    
    Co-authored-by: ncphillips <ncphillips.19@gmail.com>
    Co-authored-by: Cédric Delpoux <cedric.delpoux@gmail.com>

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,35 +1,61 @@
 const {isAbsolute} = require("path")
-const eachProps = require("each-props")
 const isValidPath = require("is-valid-path")
 
 const {getRelativePath} = require("./utils.js")
 
-function normalizePath(path, keyChain, nodeInfo) {
-  const {name, fileAbsolutePath, pathFields} = nodeInfo
+function normalizePath(path, nodeInfo) {
+  const {fileAbsolutePath} = nodeInfo
   const isString = typeof path === "string"
-
-  // Stop if pathFields is specified and do not include field name
-  if (
-    pathFields &&
-    Array.isArray(pathFields) &&
-    pathFields.indexOf(name) === -1
-  ) {
-    return
-  }
 
   // Stop if field is not a valid path
   if (!path || !isString || !isValidPath(path)) {
     // Remove field if empty to prevent Gatsby crashing
     if (isString && path.length === 0) {
-      nodeInfo.parent[name] = undefined
+      return undefined
     }
-    return
   }
 
   // Normalize path field from absolute to relative
   if (path && isString && isAbsolute(path)) {
-    nodeInfo.parent[name] = getRelativePath(fileAbsolutePath, path)
+    return getRelativePath(fileAbsolutePath, path)
   }
+
+  return path
+}
+
+function normalizeArray(arr, nodeInfo) {
+  const newArr = []
+  for (let val of arr) {
+    if (Array.isArray(val)) {
+      newArr.push(normalizeArray(val, nodeInfo))
+    } else if (typeof val === "object") {
+      newArr.push(normalizeObject(val, nodeInfo))
+    } else {
+      newArr.push(normalizePath(val, nodeInfo))
+    }
+  }
+  return newArr
+}
+
+function normalizeObject(obj, nodeInfo) {
+  const newObj = {}
+  for (let key in obj) {
+    // don't convert if pathFields is specified and this key isn't in it
+    if (
+      nodeInfo.pathFields &&
+      Array.isArray(nodeInfo.pathFields) &&
+      nodeInfo.pathFields.indexOf(key) < 0
+    ) {
+      newObj[key] = obj[key]
+    } else if (Array.isArray(obj[key])) {
+      newObj[key] = normalizeArray(obj[key], nodeInfo)
+    } else if (typeof obj[key] === "object") {
+      newObj[key] = normalizeObject(obj[key], nodeInfo)
+    } else {
+      newObj[key] = normalizePath(obj[key], nodeInfo)
+    }
+  }
+  return newObj
 }
 
 exports.onCreateNode = ({node}, pluginOptions) => {
@@ -39,7 +65,7 @@ exports.onCreateNode = ({node}, pluginOptions) => {
     return
   }
 
-  eachProps(node.frontmatter, normalizePath, {
+  node.frontmatter = normalizeObject(node.frontmatter, {
     pathFields: pluginOptions.pathFields,
     fileAbsolutePath: node.fileAbsolutePath,
   })
---
+++ src/gatsby-node.test.js
@@ -0,0 +1,90 @@
+const {onCreateNode} = require("./gatsby-node")
+const utils = require("./utils")
+
+jest.mock("./utils.js")
+
+describe("gatsby-node", () => {
+  const pluginOptions = {pathFields: ["image"]}
+
+  it("should not change a node without frontmatter", () => {
+    const node = new Node()
+
+    onCreateNode({node}, pluginOptions)
+
+    expect(node.frontmatter).toEqual({})
+  })
+
+  describe("when pluginOptions includes a pathFields", () => {
+    const pluginOptions = {pathFields: ["image"]}
+    it("should convert fields listed in pathFields", () => {
+      const node = new Node({image: "/path/to/image.jpg"})
+      const relativePath = "./path/to/image.jpg"
+      utils.getRelativePath.mockImplementation(() => relativePath)
+
+      onCreateNode({node}, pluginOptions)
+
+      expect(node.frontmatter).toEqual({image: relativePath})
+    })
+
+    it("should not convert fields not listed in pathFields", () => {
+      const node = new Node({not_an_image: "/path/to/image.jpg"})
+      utils.getRelativePath.mockImplementation(() => "./something.jpg")
+
+      onCreateNode({node}, pluginOptions)
+
+      expect(node.frontmatter).toEqual({not_an_image: "/path/to/image.jpg"})
+    })
+  })
+
+  describe("when pluginOptions does not include pathsFields", () => {
+    const pluginOptions = {pathFields: undefined}
+
+    it("should convert any string starting with '/' into a relative path", () => {
+      utils.getRelativePath.mockImplementation(() => "./something.jpg")
+      const node = new Node({pic: "/something"})
+
+      onCreateNode({node}, pluginOptions)
+
+      expect(node.frontmatter).toEqual({pic: "./something.jpg"})
+    })
+
+    it("should convert fields objects", () => {
+      utils.getRelativePath.mockImplementation(() => "./something.jpg")
+      const node = new Node({author: {portrait: "/something"}})
+
+      onCreateNode({node}, pluginOptions)
+
+      expect(node.frontmatter).toEqual({author: {portrait: "./something.jpg"}})
+    })
+
+    it("should convert arrays of paths", () => {
+      utils.getRelativePath.mockImplementation(() => "./something.jpg")
+      const node = new Node({gallery: ["/something", "/toot"]})
+
+      onCreateNode({node}, pluginOptions)
+
+      expect(node.frontmatter).toEqual({
+        gallery: ["./something.jpg", "./something.jpg"],
+      })
+    })
+
+    it("should convert fields of objects nested in arrays", () => {
+      utils.getRelativePath.mockImplementation(() => "./something.jpg")
+      const node = new Node({author: [{portrait: "/something"}]})
+
+      onCreateNode({node}, pluginOptions)
+
+      expect(node.frontmatter).toEqual({
+        author: [{portrait: "./something.jpg"}],
+      })
+    })
+  })
+})
+
+class Node {
+  constructor(frontmatter = {}) {
+    this.frontmatter = frontmatter
+    this.fileAbsolutePath = "some/file/path.md"
+    this.internal = {type: "MarkdownRemark"}
+  }
+}
