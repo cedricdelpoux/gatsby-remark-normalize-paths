ec79551c13ee3e4ccfe9f22359c744235038e714
Joseph Markus <joseph.markus@simplybusiness.co.uk>
2021-07-05 17:58:38 +0100

    Update error message to include .md file path

--- src/index.js
+++ src/index.js
@@ -1,29 +1,30 @@
-const fs = require('fs');
+const fs = require("fs")
 const {isAbsolute} = require("path")
 const visit = require(`unist-util-visit`)
 
 const {getRelativePath} = require(`./utils.js`)
 
 module.exports = ({getNode, markdownAST, markdownNode}, pluginOptions) => {
-  const { prefix } = pluginOptions;
+  const {prefix} = pluginOptions
 
-  visit(markdownAST, `image`, node => {
+  visit(markdownAST, `image`, (node) => {
     const isAbsoluteUrl = isAbsolute(node.url)
     const {absolutePath} = getNode(markdownNode.parent)
 
     if (node.url && isAbsoluteUrl) {
-      const imagePath = getRelativePath(absolutePath, node.url, prefix);
-      const fullPath = `${__dirname}/${imagePath}`;
+      const imagePath = getRelativePath(absolutePath, node.url, prefix)
+      const fullPath = `${__dirname}/${imagePath}`
 
       fs.access(fullPath, fs.F_OK, (error) => {
         if (error) {
-
-          if (error.code === 'ENOENT') {
-            console.error('Image could not be found:', imagePath);
+          if (error.code === "ENOENT") {
+            console.error(
+              `Check ${absolutePath} where the following image could not be found: ${imagePath}`
+            )
             return
           }
 
-          console.error(error);
+          console.error(error)
           return
         }
       })
