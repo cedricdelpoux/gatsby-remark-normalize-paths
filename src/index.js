cc6ef01989ca6d14a74625f1cb676248e99c5633
Joseph Markus <joseph.markus@simplybusiness.co.uk>
2021-07-05 16:45:43 +0100

    Check image path exists; display error if not

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
+const fs = require('fs');
 const {isAbsolute} = require("path")
 const visit = require(`unist-util-visit`)
 
 const {getRelativePath} = require(`./utils.js`)
 
-module.exports = ({getNode, markdownAST, markdownNode}, { prefix }) => {
+module.exports = ({getNode, markdownAST, markdownNode}, pluginOptions) => {
+  const { prefix } = pluginOptions;
+
   visit(markdownAST, `image`, node => {
     const isAbsoluteUrl = isAbsolute(node.url)
     const {absolutePath} = getNode(markdownNode.parent)
 
     if (node.url && isAbsoluteUrl) {
+      const imagePath = getRelativePath(absolutePath, node.url, prefix);
+      const fullPath = `${__dirname}/${imagePath}`;
+
+      fs.access(fullPath, fs.F_OK, (error) => {
+        if (error) {
+
+          if (error.code === 'ENOENT') {
+            console.error('Image could not be found:', imagePath);
+            return
+          }
+
+          console.error(error);
+          return
+        }
+      })
+
       node.url = getRelativePath(absolutePath, node.url, prefix)
     }
   })
